b650772f6ef1f22fc62779261356facf8642fb13
yuvalsol <yuvalsol@gmail.com>
2024-12-07 20:02:32 +0200

    extend publish batch script

--- TwitchChatToSubtitles/Publish.bat
+++ TwitchChatToSubtitles/Publish.bat
@@ -1,2 +1,8 @@
-dotnet publish -p:PublishProfile=win-x64 -p:DebugType=None -p:DebugSymbols=false
-dotnet publish -p:PublishProfile=linux-x64 -p:DebugType=None -p:DebugSymbols=false
+@echo off
+set "arguments=--configuration Release --framework net8.0 --self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:DebugType=None -p:DebugSymbols=false"
+set "publishDir=..\Output\net8.0"
+dotnet publish --output "%publishDir%\win-x64" --runtime win-x64 %arguments%
+echo.
+dotnet publish --output "%publishDir%\linux-x64" --runtime linux-x64 %arguments%
+echo.
+::pause
