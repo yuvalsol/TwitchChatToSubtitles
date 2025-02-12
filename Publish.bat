4a16c4a67278176c4d4b6c9f0d64b53996da1a82
yuvalsol <yuvalsol@gmail.com>
2025-02-12 20:39:00 +0200

    rewrite publish script

--- Publish.bat
+++ Publish.bat
@@ -1,26 +1,37 @@
 @echo off
 
-set "publishDir=Output\net8.0"
-set "win64=win-x64"
-set "linux64=linux-x64"
-set "arguments=--configuration Release --self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:DebugType=None -p:DebugSymbols=false --verbosity normal"
+set publishDir="Output\net8.0"
+set win64="win-x64"
+set linux64="linux-x64"
+set arguments="--configuration Release --self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:DebugType=None -p:DebugSymbols=false --verbosity normal"
 
 :TwitchChatToSubtitles
-set "project=TwitchChatToSubtitles"
-set "framework=--framework net8.0"
-dotnet publish %project% --output "%publishDir%\%win64%" --runtime %win64% %framework% %arguments%
-echo.
-dotnet publish %project% --output "%publishDir%\%linux64%" --runtime %linux64% %framework% %arguments%
-echo.
+call :Publish "TwitchChatToSubtitles", %publishDir%, %win64%, "net8.0", %arguments%
+call :Publish "TwitchChatToSubtitles", %publishDir%, %linux64%, "net8.0", %arguments%
 
 :TwitchChatToSubtitlesUI
-set "project=TwitchChatToSubtitlesUI"
-set "framework=--framework net8.0-windows"
-dotnet publish %project% --output "%publishDir%\%win64%" --runtime %win64% %framework% %arguments%
-echo.
-dotnet publish %project% --output "%publishDir%\%linux64%" --runtime %linux64% %framework% %arguments%
-echo.
+call :Publish "TwitchChatToSubtitlesUI", %publishDir%, %win64%, "net8.0-windows", %arguments%
+call :Publish "TwitchChatToSubtitlesUI", %publishDir%, %linux64%, "net8.0-windows", %arguments%
 
 :exit
 :: pause
 exit /b 0
+
+::----------------------------------------------------------------------
+
+:Publish
+:: Publish(project, publishDir, runtime, framework, arguments)
+setlocal
+set "project=%~1"
+set "publishDir=%~2"
+set "runtime=%~3"
+set "framework=%~4"
+set "arguments=%~5"
+
+:: echo call dotnet publish %project% --output "%publishDir%\%runtime%" --runtime %runtime% --framework %framework% %arguments%
+call dotnet publish %project% --output "%publishDir%\%runtime%" --runtime %runtime% --framework %framework% %arguments%
+echo.
+
+endlocal
+
+goto :eof
