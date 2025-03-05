bf75486750cd579f436d32afc3e200ca8e462741
yuvalsol <yuvalsol@gmail.com>
2025-03-05 18:58:04 +0200

    add comments to settings file

--- TwitchChatToSubtitlesUI/UISettings.cs
+++ TwitchChatToSubtitlesUI/UISettings.cs
@@ -5,13 +5,18 @@ namespace TwitchChatToSubtitlesUI;
 [Serializable]
 internal class UISettings
 {
+    public string _SubtitlesType { get { return $"// {SubtitlesType}"; } }
     public SubtitlesType SubtitlesType { get; set; }
     public bool ColorUserNames { get; set; }
     public bool RemoveEmoticonNames { get; set; }
     public bool ShowTimestamps { get; set; }
+    public string _SubtitlesLocation { get { return $"// {SubtitlesLocation}"; } }
     public SubtitlesLocation SubtitlesLocation { get; set; }
+    public string _SubtitlesFontSize { get { return $"// {SubtitlesFontSize}"; } }
     public SubtitlesFontSize SubtitlesFontSize { get; set; }
+    public string _SubtitlesRollingDirection { get { return $"// {SubtitlesRollingDirection}"; } }
     public SubtitlesRollingDirection SubtitlesRollingDirection { get; set; }
+    public string _SubtitlesSpeed { get { return $"// {SubtitlesSpeed}"; } }
     public SubtitlesSpeed SubtitlesSpeed { get; set; }
     public decimal TimeOffset { get; set; }
     public decimal SubtitleShowDuration { get; set; }
