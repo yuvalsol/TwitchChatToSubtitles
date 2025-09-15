7943bac52710c345fdef8fd76dd3e6621870042a
yuvalsol <yuvalsol@gmail.com>
2025-09-15 16:57:16 +0300

    fix RegexExtensions

--- TwitchChatToSubtitles.Library/Extensions/RegexExtensions.cs
+++ TwitchChatToSubtitles.Library/Extensions/RegexExtensions.cs
@@ -2,9 +2,25 @@
 
 public static partial class RegexExtensions
 {
+    public static void Remove(this Regex regex, StringBuilder input)
+    {
+        regex.Remove(input, input.ToString());
+    }
+
+    public static void Remove(this Regex regex, StringBuilder input, string inputText)
+    {
+        foreach (var match in regex.Matches(inputText).OrderByDescending(m => m.Index))
+            input.Remove(match.Index, match.Length);
+    }
+
     public static void Replace(this Regex regex, StringBuilder input, string replacement)
     {
-        foreach (var match in regex.Matches(input.ToString()).Cast<Match>().OrderByDescending(m => m.Index))
+        regex.Replace(input, input.ToString(), replacement);
+    }
+
+    public static void Replace(this Regex regex, StringBuilder input, string inputText, string replacement)
+    {
+        foreach (var match in regex.Matches(inputText).OrderByDescending(m => m.Index))
         {
             input.Remove(match.Index, match.Length);
             input.Insert(match.Index, replacement);
